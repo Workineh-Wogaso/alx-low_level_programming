af1087a7825dd9831505fbca85ce3e6249bee76b
HaxorLeoul <workinehw94@gmail.com>
2022-03-11 10:11:30 -0800

    7-print_tebahpla.c

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -6,9 +6,9 @@
  **/
 int main(void)
 {
-        char c;
-        for (c = 'z'; c >= 'a'; c--)
-                putchar(c);
+    char c;
+    for (c = 'z'; c >= 'a'; c--)
+          putchar(c);
         putchar('\n');
         return (0);
 }
