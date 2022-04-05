d91188becef16d9ff7e9bf1ad5c0e1c827b59bbe
HaxorLeoul <workinehw94@gmail.com>
2022-04-05 13:07:27 -0700

    Commit

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -55,9 +55,9 @@ int num_words(char *str)
  *Return: pointer to the array of splitted words
  */
 
-OBOBOBOBOBOBOBOBOBchar **strtow(char *str)
+char **strtow(char *str)
 {
-OBOBOB	char **split;
+	char **split;
 	int i, j = 0, temp = 0, size = 0, words = num_words(str);
 
 	if (words == 0)
