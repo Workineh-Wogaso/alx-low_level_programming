ff8a343e60cbce94094b622cf4a5e1c1cbb42ccd
HaxorLeoul <workinehw94@gmail.com>
2022-05-02 03:28:52 -0700

    0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,42 +1,35 @@
 #include "main.h"
+#include <stddef.h>
+
 /**
-* read_textfile - check the code for Holberton School students.
-* @filename: file to read and write
-* @letters: number of letters to read and write.
-* Return: letters printed
-*/
+ * read_textfile - reads a text file and prints it to the standard output
+ * @filename: name of the file
+ * @letters:  number of letters to be printed
+ *
+ * Return: number of letters read and printed
+ */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	ssize_t nletters;
-	int file;
-	char *text;
+	int file, n_read, wrote;
+	char *buffer;
 
-	if (!filename)
+	buffer = malloc(sizeof(*buffer) * (letters + 1));
+	if (filename == NULL || buffer == NULL)
+	{
+		free(buffer);
 		return (0);
-	text = malloc(sizeof(char) * letters + 1);
-	if (text == NULL)
-		return (0);
+	}
 	file = open(filename, O_RDONLY);
 	if (file == -1)
-	{
-		free(text);
 		return (0);
-	}
-	nletters = read(file, text, sizeof(char) * letters);
-	if (nletters == -1)
-	{
-		free(text);
-		close(file);
+	n_read = read(file, buffer, letters);
+	if (n_read == -1)
 		return (0);
-	}
-	nletters = write(STDOUT_FILENO, text, nletters);
-	if (nletters == -1)
-	{
-		free(text);
-		close(file);
+	buffer[n_read] = '\0';
+	wrote = write(STDOUT_FILENO, buffer, n_read);
+	if (wrote != n_read)
 		return (0);
-	}
-	free(text);
+	free(buffer);
 	close(file);
-	return (nletters);
+	return (n_read);
 }
